5e0a73eb19e17002e55c2865361742a742a641e8
TrekHub <teddyolweny@gmail.com>
2021-10-05 12:23:47 +0300

    add files to the remote repo

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>Welcome To Tracky</h1>
+ 
+</template>
+
+<script>
+export default {
+  name: 'Header',
+ 
+}
+</script>
+
+
+<style scoped>
+h1 {
+  font-size: 40px;
+  text-align: center;
+}
+</style>
